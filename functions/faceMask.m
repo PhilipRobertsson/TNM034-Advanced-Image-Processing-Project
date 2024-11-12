81c208dffc41d5185556bcd4497081a22e38806e
PhilipRobertsson <phiro138@student.liu.se>
2024-11-12 11:41:00 +0100

    Total restructure of faceMask.m

--- functions/faceMask.m
+++ functions/faceMask.m
@@ -1,21 +1,23 @@
 function [maskOutput] = faceMask(imageInput)
 %   FACEMASK Summary of this function goes here
 %   Function to create a mask of the face in the image.
-%   Based on Skin Detection and Segmentation of Human Face in Color Images
+%   Based on sSkin Detection and Segmentation of Human Face in Color
+%   Images
 %   written by Baozhu Wang, Xiuying Chang, Cuixiang Liu.
 
 % Image to work on
 workloadImage = imageInput;
 
-% HSV images
-[hueImage, satImage, valImage] = rgb2hsv(workloadImage);
+% Avrage of the color channels
+AR = workloadImage(:,:,1);
+AG = workloadImage(:,:,2);
+AB = workloadImage(:,:,3);
 
-subplot(1,3,1)
-imshow(hueImage);
-subplot(1,3,2);
-imshow(satImage);
-subplot(1,3,3);
-imshow(valImage);
+for i = 1 : size(workloadImage,1)
+    for j = 1 : size(workloadImage,2)
+
+    end
+end
 
 end
 
