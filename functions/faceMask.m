5a3379d49cb2376581de6a428f4d1a51cff438c7
PhilipRobertsson <phiro138@student.liu.se>
2024-11-20 16:49:41 +0100

    LightComp is own file
    Added LightCompensation as it's own function, gray world works well, still seems to be problems with white patch.

--- functions/faceMask.m
+++ functions/faceMask.m
@@ -7,51 +7,19 @@ function [maskOutput] = faceMask(imageInput)
 % Image to work on
 workloadImage = im2double(imageInput);
 
-% Avrage of the color channels
+% Color space change with image
+YCbCr = rgb2ycbcr(workloadImage);
+HSV = rgb2hsv(workloadImage);
+
+% Split RGB into separate channels
 R = workloadImage(:,:,1);
 G = workloadImage(:,:,2);
 B = workloadImage(:,:,3);
-avgR = mean(R(:));
-avgG = mean(G(:));
-avgB = mean(B(:));
-avgGray = (avgR+avgG+avgB)/3;
-
-% Adjustment factors
-aR = avgGray/avgR;
-aG = avgGray/avgG;
-aB = avgGray/avgB;
-
-% Adjustment of RGB channels
-adR = R * aR;
-adG = G * aG;
-adB = B * aB;
-
-% Concentrate all adjusted channels to a single image
-adWorkloadImage = cat(3, adR, adG, adB);
-
-maximumValue = max(max(adWorkloadImage));
-[x,y] = find(adWorkloadImage == maximumValue);
-
-maxR = maximumValue*R(x(1), y(1));
-maxG = maximumValue*G(x(1), y(1));
-maxB = maximumValue*B(x(1), y(1));
-
-alpha = maxG(:,:,1)/maxR(:,:,1);
-beta = maxG(:,:,1)/maxB(:,:,1);
-
-whitePatchR = alpha * R;
-whitePatchG = 1.01 * G;
-whitePatchB = beta * B;
-
-whitepatchWorkloadImage = cat(3, whitePatchR, whitePatchG, whitePatchB);
-
-% Color space change with light compansated image
-YCbCr = rgb2ycbcr(whitepatchWorkloadImage);
-HSV = rgb2hsv(adWorkloadImage);
 
 % Split YCbCr image into separate channels
 Y = YCbCr(:,:,1);
 Cb = YCbCr(:,:,2);
+Cg = (128/255) - (81.085/255)*R + (122/255)*G - (30.915/255)*B;
 Cr = YCbCr(:,:,3);
 
 % Split HSV image into separate channels
@@ -59,19 +27,12 @@ H = HSV(:,:,1);
 S = HSV(:,:,2);
 V = HSV(:,:,3);
 
-
-maxCr = max(Cr);
-normCr = Cr./maxCr;
-
 % YCbCr skin mask
-maskCbR = R - Cb.*1.1;
-maskCbRCr = maskCbR.*normCr;
-maskCbRCr = maskCbRCr >=0.2;
-
-
+maskCbR = (R - Cg).*(Cr.*1.0);
+maskCbR = (maskCbR >= 0.05);
 
 % Edge mask
-workloadImageGray = im2gray(Cr);
+workloadImageGray = im2gray(workloadImage);
 Sobx = [-1,-2,-1;0,0,0;1, 2, 1];
 Soby = [-1,0,1;-2,0,2;-1, 0, 1];
 
@@ -79,69 +40,63 @@ imgSobx =filter2(Sobx,workloadImageGray,'same');
 imgSoby = filter2(Soby,workloadImageGray,'same');
 
 maskEdge = sqrt(imgSobx.^2+ imgSoby.^2);
-maskEdge = (maskEdge >= 0.1);
+maskEdge = (maskEdge > 0.4);
 
-maskComb = (maskCbRCr == 1) & (maskEdge == 0);
+maskComb = (maskCbR == 1) & (maskEdge == 0);
 
-SE1=strel("rectangle", [17,17]);
+SE1=strel("rectangle", [10,10]);
 maskComb = imopen(maskComb,SE1);
-SE2=strel("disk",[16]);
+SE2=strel("disk",[10]);
 maskComb = imclose(maskComb,SE2);
 
 cropped = bwareafilt(maskComb,1);
 
-SE1=strel("rectangle", [17,17]);
+SE1=strel("rectangle", [12,12]);
 cropped = imopen(cropped,SE1);
-SE2=strel("disk",[40]);
+SE2=strel("disk",[50]);
 cropped = imclose(cropped,SE2);
 
-maskedImage = bsxfun(@times, whitepatchWorkloadImage, cast(cropped, 'like', whitepatchWorkloadImage));
+maskedImage = bsxfun(@times, workloadImage, cast(cropped, 'like', workloadImage));
 
 
 % Temporary image viewing
-subplot(3,4,1);
-imshow(Y);
-title('Y');
+%subplot(3,4,1);
+%imshow(Y);
+%title('Y');
 
-subplot(3,4,2);
-imshow(Cb);
-title('Cb');
+%subplot(3,4,2);
+%imshow(Cb);
+%title('Cb');
 
-subplot(3,4,3);
-imshow(Cr);
-title('Cr');
+%subplot(3,4,3);
+%imshow(Cr);
+%title('Cr');
 
-subplot(3,4,4);
-imshow(YCbCr);
-title('YCbCr Image');
+%subplot(3,4,4);
+%imshow(Cg);
+%title('Cg');
 
-subplot(3,4,5);
-imshow(maskCbRCr);
-title('(R - Cb) mask');
+%subplot(3,4,5);
+%imshow(maskCbR);
+%title('(R - Cb) mask');
 
-subplot(3,4,6);
-imshow(maskEdge);
-title('Edge mask');
+%subplot(3,4,6);
+%imshow(maskEdge);
+%title('Edge mask');
 
-subplot(3,4,7);
-imshow(cropped);
-title('Combined YCbCr and edge masks');
+%subplot(3,4,7);
+%imshow(cropped);
+%title('Combined YCbCr and edge masks');
 
-subplot(3,4,8)
-imshow(Cr./maxCr);
-title('CrmaxCr')
 
-subplot(3,4,9);
-imshow(workloadImage);
-title('Original image');
+%subplot(3,4,9);
+%imshow(workloadImage);
+%title('Original image');
 
-subplot(3,4,10);
-imshow(whitepatchWorkloadImage);
-title('Light compensated image');
+%subplot(3,4,11);
+%imshow(maskedImage);
+%title('Masked light compensated image');
 
-subplot(3,4,11);
-imshow(maskedImage);
-title('Masked light compensated image');
-
+maskOutput = maskedImage;
 end
 
---
+++ functions/lightCompensation.m
@@ -0,0 +1,46 @@
+function [outputImage] = lightCompensation(inputImage)
+%LIGHTCOMPENSATION Summary of this function goes here
+workloadImage = im2double(inputImage);
+
+% Split channels R, G, B
+R = workloadImage(:,:,1);
+G = workloadImage(:,:,2);
+B = workloadImage(:,:,3);
+
+% Find avarage values for all channels, gray world
+avgR = mean(R(:));
+avgG = mean(G(:));
+avgB = mean(B(:));
+
+a = avgG / avgR;
+b = avgG / avgB;
+
+adR = a * R;
+adG = G;
+adB = b * B;
+
+% white patch
+numRevPixels = floor((size(workloadImage,1)) * 0.5); % 10% of pixels
+
+maxRed = max(maxk(R,numRevPixels,2));
+maxGreen = max(maxk(G,numRevPixels,2));
+maxBlue = max(maxk(B,numRevPixels,2));
+
+meanMax = mean(cat(2, maxRed, maxGreen, maxBlue),"all");
+
+mR=  R * meanMax;
+mG = G * meanMax;
+mB = B * meanMax;
+
+alpha = mG / mR;
+beta = mG / mB;
+
+wpR = alpha * R;
+wpG = G;
+wpB = beta * B;
+
+outputImage = cat(3,wpR,wpG,wpB);
+
+
+end
+
