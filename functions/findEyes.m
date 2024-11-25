b3819844aa9656e7c5cb26bff4e61c3a271bd499
PhilipRobertsson <phiro138@student.liu.se>
2024-11-25 12:03:54 +0100

    Getting closer to a working eyemask
    Added several morphological operations to segment the eyes

--- functions/findEyes.m
+++ functions/findEyes.m
@@ -12,10 +12,34 @@ Cb = YCbCr(:,:,2);
 Cr = YCbCr(:,:,3);
 Cg = (128/255) - (81.085/255)*workloadImage(:,:,1) + (122/255)*workloadImage(:,:,2) - (30.915/255)*workloadImage(:,:,3);
 
-Eyes = 1.05 * pow2(Cg) - pow2(Cr);
-Eyes = Eyes.*workloadMask;
-Eyes = Eyes - 0.01*(Cg.*workloadMask);
+Cr2 = Cr+Cr;
+Cb2 = Cb+Cb;
 
- res = Eyes;
+EyeMapC = (Cb2 ./ Cr2) - Y;
+EyeMapC = (EyeMapC .* workloadMask) - Cb;
+EyeMapC = 2*EyeMapC;
+EyeMapC = EyeMapC - 0.6*im2gray(faceOnly);
+EyeMapC = (EyeMapC > 0.65);
+
+SE1 = strel("disk",15);
+EyeMapC = imdilate(EyeMapC,SE1);
+
+EyeMapC = EyeMapC - (Cr>0.5);
+
+SE1 = strel("disk",15);
+EyeMapC = imdilate(EyeMapC,SE1);
+
+SE2 = strel("disk",10);
+EyeMapC = imopen(EyeMapC,SE2);
+
+
+
+%[centers, radii] = imfindcircles(EyeMapC, [6 10], 'ObjectPolarity', 'dark', 'Sensitivity', 0.97);
+
+%hold on
+    %viscircles(centers, radii);
+%hold off
+
+ res = EyeMapC;
 end
 
