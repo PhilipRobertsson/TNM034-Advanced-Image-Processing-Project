0040ca68777f4aa83bcbbeeb408e70ab8168e49c
Caity <caiwu993@student.liu.se>
2024-12-12 23:28:12 +0100

    Update eigenfaces.m
    changed how many eigenfaces are saved, after a ahaa!-moment

--- functions/eigenfaces.m
+++ functions/eigenfaces.m
@@ -1,7 +1,7 @@
 function [weightsEigenfaces, meanFace, eigenFaces] = eigenfaces(imagesDB1)
 % EIGENFACES - A MATLAB implementation of face recognition using eigenfaces.
 % This script computes eigenfaces using PCA. The weights from DB1 are
-% returned
+% returned.
 
 % Compute mean face
 meanFace = mean(imagesDB1, 2);
@@ -14,20 +14,22 @@ transposeOfA = bigPhi'; % Transpose of the matrix
 B = transposeOfA * bigPhi; % Covariance matrix
 [eigenVectors, eigenValues] = eig(B); % Eigen decomposition
 
-% Compute eigenfaces
-eigenFaces = zeros(size(bigPhi, 1), size(bigPhi, 2));
-for i = 1:size(bigPhi, 2)
-    eigenface = bigPhi * eigenVectors(:, i);
+% Sort eigenvalues in descending order and rearrange eigenvectors accordingly
+[eigenValuesSorted, indices] = sort(diag(eigenValues), 'descend');
+eigenVectors = eigenVectors(:, indices);
+
+% Select the 5 most important eigenfaces
+numImportantEigenfaces = 5;
+selectedEigenVectors = eigenVectors(:, 1:numImportantEigenfaces);
+
+% Compute the top 5 eigenfaces
+eigenFaces = zeros(size(bigPhi, 1), numImportantEigenfaces);
+for i = 1:numImportantEigenfaces
+    eigenface = bigPhi * selectedEigenVectors(:, i);
     eigenFaces(:, i) = eigenface / norm(eigenface); % Normalize eigenfaces
 end
 
-% Display the mean face
-% figure;
-%mean_face_img = reshape(mean_face, size(cropped));
-%imshow(mean_face_img, []);
-%title('Mean Face');
-
-% Compute weights for training images
+% Compute weights for training images using the selected eigenfaces
 weightsEigenfaces = eigenFaces' * bigPhi;
 
 end
